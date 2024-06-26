b442b34213394f6213b6898c04a30c1ce93aafe7
MayanLavi <mayan.lavi@campus.technion.ac.il>
2024-06-26 11:00:49 +0300

    Update clean-git.sh

--- HomeworkTools/HW1/clean-git.sh
+++ HomeworkTools/HW1/clean-git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ ! -z "$(git status --porcelain -u no)" ]]; then
+if [[ ! -z "$(git status --porcelain -uno | grep -v -e 'cmake-build-debug' -e '.idea' ) ]]; then
     echo "Cannot clean git while repo files were changed but not comitted" >&2;
     exit -1;
 fi
