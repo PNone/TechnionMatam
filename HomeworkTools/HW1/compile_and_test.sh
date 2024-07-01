c65645f9a322f4857aad6e86859ce12bdef222bf
PNone <urikita8@gmail.com>
2024-07-01 21:01:23 +0300

    Add invalid operation with valid params test. improve compile and test

--- HomeworkTools/HW1/compile_and_test.sh
+++ HomeworkTools/HW1/compile_and_test.sh
@@ -1,2 +1,3 @@
+git submodule update --init --recursive --remote
 g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o mtm_blockchain *.cpp
 python3 ./MatamGenericTester/run_tests.py ./mtm_blockchain ./TechnionMatam/HW1/tests/custom_tests.json
