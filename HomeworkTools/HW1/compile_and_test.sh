dbe02f2581abc45e5d1090f027bca66306ad76ef
PNone <urikita8@gmail.com>
2024-07-01 22:46:22 +0300

    Add Shabang to HW1 compile and test

--- HomeworkTools/HW1/compile_and_test.sh
+++ HomeworkTools/HW1/compile_and_test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git submodule update --init --recursive --remote
 g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o mtm_blockchain *.cpp
 python3 ./MatamGenericTester/run_tests.py ./mtm_blockchain ./TechnionMatam/HW1/tests/custom_tests.json
