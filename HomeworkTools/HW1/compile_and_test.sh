6c17ba78cfc57480a253f11a5d6ffbba76643801
PNone <urikita8@gmail.com>
2024-07-01 20:31:59 +0300

    Add Compile and Test Script

---
+++ HomeworkTools/HW1/compile_and_test.sh
@@ -0,0 +1,2 @@
+g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o mtm_blockchain *.cpp
+python3 ./MatamGenericTester/run_tests.py ./mtm_blockchain ./TechnionMatam/HW1/tests/custom_tests.json
