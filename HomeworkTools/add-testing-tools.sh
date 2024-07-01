d05f9d006dd5cc11034986772590a3d5897cbd8d
PNone <urikita8@gmail.com>
2024-07-01 20:29:31 +0300

    Create https clone script

--- HomeworkTools/add-testing-tools.sh
+++ HomeworkTools/add-testing-tools.sh
@@ -1,2 +1,2 @@
-git submodule add git@github.com:PNone/TechnionMatam.git
-git submodule add git@github.com:PNone/MatamGenericTester.git
+git submodule add https://github.com/PNone/TechnionMatam.git
+git submodule add https://github.com/PNone/MatamGenericTester.git
