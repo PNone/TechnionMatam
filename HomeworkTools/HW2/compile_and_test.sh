48cb4acd9e2f4a4d1e3af5414dd7acaea25d5781
PNone <urikita8@gmail.com>
2024-07-09 13:18:00 +0300

    Fix compilation due to h files path relative to test file

--- HomeworkTools/HW2/compile_and_test.sh
+++ HomeworkTools/HW2/compile_and_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 git submodule update --init --recursive --remote
-g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o MvidiaTest MataMvidia.cpp Matrix.cpp Utilities.cpp ./TechnionMatam/HW2/tests/tests.cpp
+# Compiles users assignment files and our tests file, using . as "include's path"
+g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o MvidiaTest MataMvidia.cpp Matrix.cpp Utilities.cpp -I . ./TechnionMatam/HW2/tests/tests.cpp
 python3 ./MatamGenericTester/run_tests.py ./MvidiaTest ./TechnionMatam/HW2/tests/custom_tests.json
