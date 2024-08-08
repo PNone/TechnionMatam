866d3990fbcc520cd431c0c691ac0df7d09bcc3a
PNone <urikita8@gmail.com>
2024-08-08 19:59:56 +0300

    Fix test runner HW3 and HW4 mixup

--- HomeworkTools/HW3/compile_and_test.sh
+++ HomeworkTools/HW3/compile_and_test.sh
@@ -2,5 +2,5 @@
 
 git submodule update --init --recursive --remote
 # Compiles users assignment files and our tests file, using . as "include's path"
-g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o MatamStoryTest MatamStory.cpp Events/*.cpp Players/*.cpp -I . ./TechnionMatam/HW4/tests/tests.cpp
-python3 ./MatamGenericTester/run_tests.py ./MatamStoryTest ./TechnionMatam/HW4/tests/custom_tests.json
+g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o TaskManagerTest TaskManager.cpp Task.cpp Person.cpp -I . ./TechnionMatam/HW3/tests/tests.cpp
+python3 ./MatamGenericTester/run_tests.py ./TaskManagerTest ./TechnionMatam/HW3/tests/custom_tests.json
