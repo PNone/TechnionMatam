0ad21e705d8f373cf3049ab528c822f67b5fb95f
PNone <urikita8@gmail.com>
2024-08-09 18:57:13 +0300

    HW4 - match compilation command to instructions

--- HomeworkTools/HW4/compile_and_test.sh
+++ HomeworkTools/HW4/compile_and_test.sh
@@ -2,5 +2,5 @@
 
 git submodule update --init --recursive --remote
 # Compiles users assignment files and our tests file, using . as "include's path"
-g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o MatamStoryTest *.cpp Events/*.cpp Players/*.cpp
+g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o MatamStoryTest *.cpp Events/*.cpp Players/*.cpp -I. -I./Players -I./Events
 python3 ./MatamGenericTester/run_tests.py ./MatamStoryTest ./TechnionMatam/HW4/tests/custom_tests.json
