936b4711d514fff92890e5e4dda02b4e947a9853
PNone <urikita8@gmail.com>
2024-08-19 21:32:48 +0300

    Fix HW5 test running

--- HomeworkTools/HW5/compile_and_test.sh
+++ HomeworkTools/HW5/compile_and_test.sh
@@ -3,4 +3,4 @@
 git submodule update --init --recursive --remote
 export MATAM_TESTER_TRIMR_SPACES=1
 export MATAM_TESTER_IGNORE_EMPTY_LINES=1
-python3 ./MatamGenericTester/run_tests.py ./TechnionMatam/HW5/tests/custom_tests.json python3 ./enigma.py
+python3 ./MatamGenericTester/run_tests.py ./TechnionMatam/HW5/tests/custom_tests.json /usr/bin/python3 ./enigma.py
