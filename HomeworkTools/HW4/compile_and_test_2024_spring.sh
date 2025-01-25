2a1d2b1a75cde9661f8e6aa146d12ae2c19c5fda
PNone <urikita8@gmail.com>
2025-01-26 00:28:33 +0200

    Backup and separate HW4 2024 spring and winter 2025 tests

---
+++ HomeworkTools/HW4/compile_and_test_2024_spring.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git submodule update --init --recursive --remote
+# Compiles users assignment files and our tests file, using . as "include's path"
+g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o MatamStoryTest *.cpp Events/*.cpp Players/*.cpp -I. -I./Players -I./Events
+python3 ./MatamGenericTester/run_tests.py ./TechnionMatam/HW4/tests_2024_spring/custom_tests.json ./MatamStoryTest
